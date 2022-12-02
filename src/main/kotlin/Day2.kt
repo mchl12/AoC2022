54d14a41575f438ae556ba3c4c8b338b9d8d457e
mchl12 <12mchl.vdl@gmail.com>
2022-12-02 11:38:20 +0100

    Day 2 part 2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -21,8 +21,32 @@ fun scoreOfMatch(oMove: Char, myMove: Char): Int {
     throw IllegalArgumentException()
 }
 
+fun moveToMake(oMove: Char, result: Char): Char {
+    if (oMove == 'A' && result == 'X') return 'Z'
+    if (oMove == 'A' && result == 'Y') return 'X'
+    if (oMove == 'A' && result == 'Z') return 'Y'
+    if (oMove == 'B' && result == 'X') return 'X'
+    if (oMove == 'B' && result == 'Y') return 'Y'
+    if (oMove == 'B' && result == 'Z') return 'Z'
+    if (oMove == 'C' && result == 'X') return 'Y'
+    if (oMove == 'C' && result == 'Y') return 'Z'
+    if (oMove == 'C' && result == 'Z') return 'X'
+
+    throw IllegalArgumentException()
+}
+
+fun scoreOfMatch2(result: Char) = when (result) {
+    'X' -> 0
+    'Y' -> 3
+    'Z' -> 6
+    else -> throw IllegalArgumentException()
+}
+
 fun calculateScore(matches: List<Pair<Char, Char>>) =
     matches.sumOf { scoreOfMatch(it.first, it.second) + scoreOfMove(it.second) }
+
+fun calculateScore2(matches: List<Pair<Char, Char>>) =
+    matches.sumOf { scoreOfMatch2(it.second) + scoreOfMove(moveToMake(it.first, it.second)) }
 
 fun main() {
     println(System.getProperty("user.dir"))
@@ -33,4 +57,7 @@ fun main() {
 
     val score = calculateScore(matches)
     println(score)
+
+    val score2 = calculateScore2(matches)
+    println(score2)
 }
