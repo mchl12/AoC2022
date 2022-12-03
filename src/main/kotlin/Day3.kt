f9c3a137573066f7320dec94b1f1c831579ab974
mchl12 <12mchl.vdl@gmail.com>
2022-12-03 13:49:08 +0100

    Day 3 part 2

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import kotlin.collections.intersect
 
 fun getPriority(item: Char): Int {
     val code = item.code
@@ -10,11 +11,22 @@ fun getPriority(item: Char): Int {
 }
 
 fun main() {
-    val score: Int = File("./build/resources/main/day3.in")
-        .readLines()
+    val lines = File("./build/resources/main/day3.in").readLines()
+
+    val score1: Int = lines
         .map { Pair(it.substring(0, it.length / 2).toSet(), it.substring(it.length / 2, it.length).toSet()) }
         .map { it.first.intersect(it.second).toList()[0] }
         .sumOf { getPriority(it) }
 
-    println(score)
+    println(score1)
+
+    val score2 = lines
+        .asSequence()
+        .map(String::toSet)
+        .chunked(3)
+        .map { it[0].intersect(it[1]).intersect(it[2]) }
+        .map { it.toList()[0] }
+        .sumOf { getPriority(it) }
+
+    println(score2)
 }
