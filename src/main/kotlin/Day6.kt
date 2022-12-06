5242cfea73edefaa70d8a57ae8262584ff3ef9fb
mchl12 <12mchl.vdl@gmail.com>
2022-12-06 16:49:00 +0100

    Day 6 4x speedup

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,19 +1,64 @@
 import java.io.File
+import kotlin.system.measureTimeMillis
+
+fun getFirstDistinctN(codeInt: IntArray, n: Int): Int {
+    val letterIncluded = IntArray(26) { 0 }
+    var counter = 0
+    for (i in 0 until n) {
+        val v = codeInt[i]
+        if (letterIncluded[v] == 0)
+            counter++
+        letterIncluded[v]++
+    }
+    var i = n
+    while (counter < n) {
+        val v = codeInt[i - n]
+
+        when (letterIncluded[v]) {
+            0 -> {}
+            1 -> {
+                counter--
+                letterIncluded[v] = 0
+            }
+            else -> letterIncluded[v]--
+        }
+
+        val w = codeInt[i]
+        if (letterIncluded[w] == 0)
+            counter++
+
+        letterIncluded[w]++
+
+        i++
+    }
+
+    return i
+}
 
 fun main() {
     val lines = File("./build/resources/main/day6.in").readLines()
 
     val code = lines[0]
 
-    val indexp1 = code
-        .windowedSequence(4)
-        .indexOfFirst { it.toSet().size == 4 }
+    // first slow version
+//    val indexp1 = code
+//        .windowedSequence(4)
+//        .indexOfFirst { it.toSet().size == 4 }
+//
+//    println(indexp1 + 4)
+//
+//    val indexp2 = code
+//        .windowedSequence(14)
+//        .indexOfFirst { it.toSet().size == 14 }
+//
+//    println(indexp2 + 14)
 
-    println(indexp1 + 4)
+    // fast version
+    val time = measureTimeMillis {
+        val codeInt: IntArray = code.map { c -> c - 'a' }.toIntArray()
 
-    val indexp2 = code
-        .windowedSequence(14)
-        .indexOfFirst { it.toSet().size == 14 }
-
-    println(indexp2 + 14)
+        println(getFirstDistinctN(codeInt, 4))
+        println(getFirstDistinctN(codeInt, 14))
+    }
+    println("time = ${time}ms")
 }
