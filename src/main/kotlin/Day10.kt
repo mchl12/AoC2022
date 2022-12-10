62f723f14ad49b2abda012b8a5a2b1f5245e56df
mchl12 <12mchl.vdl@gmail.com>
2022-12-10 14:54:56 +0100

    Day10 part 2

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import kotlin.math.abs
 
 fun main() {
     val lines = File("./build/resources/main/day10.in").readLines()
@@ -27,7 +28,20 @@ fun main() {
     val sum = (values[20] * 20 + values[60] * 60 + values[100] * 100
         + values[140] * 140 + values[180] * 180 + values[220] * 220)
 
-    println("${values[20]} ${values[60]} ${values[100]} ${values[140]} ${values[180]} ${values[220]}")
-
     println(sum)
+
+    val image = List<CharArray>(6) { CharArray(40) { '.' } }
+
+    for (y in 0 until 6) {
+        for (x in 0 until 40) {
+            val index = y * 40 + x + 1
+            val spritePos = values[index]
+            if (abs(x - spritePos) <= 1)
+                image[y][x] = '#'
+        }
+    }
+
+    for (line in image) {
+        println(line)
+    }
 }
