670a660c16b75f6e1d31eab6174d3fd620ed8a18
mchl12 <12mchl.vdl@gmail.com>
2022-12-16 15:46:20 +0100

    Day 16 part 1

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -1,6 +1,12 @@
 import java.io.File
+import java.util.*
 
-val memo: List<List<IntArray>> = listOf() // minutes, position, open valves
+var valveMap: Map<String, Int> = mapOf()
+var graph: List<MutableList<Int>> = listOf()
+var flows: IntArray = IntArray(0)
+var dists: List<IntArray> = listOf()
+var valvesWithFlow: Int = 0
+var memo: List<List<IntArray>> = listOf()
 
 fun main() {
     val lines = File("./build/resources/main/day16.in").readLines()
@@ -8,16 +14,16 @@ fun main() {
 
     val regexedLines = lines.map { regex.matchEntire(it)!! }
 
-    val valveMap = regexedLines
-        .mapIndexed { index, match -> Triple(match.groupValues[2].toInt(), match.groupValues[1], index) }
+    valveMap = regexedLines
+        .map { match -> Pair(match.groupValues[2].toInt(), match.groupValues[1]) }
         .sortedByDescending { it.first }
-        .map { Pair(it.second, it.third) }
+        .mapIndexed { i, p -> Pair(p.second, i) }
         .associate { it }
 
     println(valveMap)
 
-    val graph = List(valveMap.size) { mutableListOf<Int>() }
-    val flows = IntArray(valveMap.size) { 0 }
+    graph = List(valveMap.size) { mutableListOf() }
+    flows = IntArray(valveMap.size) { 0 }
 
     for ((name, flowStr, tunnelList) in regexedLines.map(MatchResult::destructured)) {
         val index = valveMap[name]!!
@@ -32,12 +38,55 @@ fun main() {
         flows[index] = flow
     }
 
-    // bfs for each node to get distance from any node to any other node
-    // calculate number of valves with a flow
-    // initialize the memo by [minutesLeft][currentLocation][openedValves]
-    // where openedValves is a bitmap that has a bit for each valve with a flow rate > 0
-    // should result in at most 30 * 14 * 2^14 states (14 is number of valves with flow rate in real input)
-    // with top down we don't even visit all
-    // calculating one state is just a comparison between all 14 possible unvisited states
-    // top down function returns the total flow rate
+    dists = List(valveMap.size) { IntArray(valveMap.size) { -1 } }
+    val queue: Queue<Int> = LinkedList()
+
+    for (i in valveMap.values) {
+        val distArray = dists[i]
+        queue.add(i)
+        distArray[i] = 0
+        while (!queue.isEmpty()) {
+            val j = queue.remove()
+            val dist = distArray[j]
+            for (n in graph[j]) {
+                if (distArray[n] >= 0)
+                    continue
+                queue.add(n)
+                distArray[n] = dist + 1
+            }
+        }
+    }
+
+    valvesWithFlow = flows.indexOfFirst { it == 0 }
+
+    memo = List(31) { List(valveMap.size) { IntArray(1 shl valvesWithFlow) { -1 } } }
+
+    val result = part1(30, valveMap["AA"]!!, 0)
+
+    println(result)
+}
+
+fun part1(minutes: Int, position: Int, valveBitmap: Int): Int {
+    if (memo[minutes][position][valveBitmap] >= 0)
+        return memo[minutes][position][valveBitmap]
+
+    var result = 0
+
+    for (i in 0 until valvesWithFlow) {
+        if ((valveBitmap and (1 shl i)) != 0)
+            continue
+        val time = dists[position][i] + 1
+        val newMinutes = minutes - time
+        if (newMinutes <= 0)
+            continue
+        val newPosition = i
+        val newValveBitmap = valveBitmap or (1 shl i)
+        val pressureRelease = newMinutes * flows[i]
+        val score = part1(newMinutes, newPosition, newValveBitmap) + pressureRelease
+        if (score > result)
+            result = score
+    }
+
+    memo[minutes][position][valveBitmap] = result
+    return result
 }
